b51a9b1dd225ec3c765d627eb47dab5d24a3d3e4
evilgamer163 <evilgamestm@gmail.com>
2020-05-26 02:05:26 +0400

    add switch card image

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 // DOM elements
 const leftMenu = document.querySelector('.left-menu'),
-    hamburger = document.querySelector('.hamburger');
+    hamburger = document.querySelector('.hamburger'),
+    tvCardImg = document.querySelectorAll('.tv-card__img');
 
 
-// listeners
+/* <-- listeners --> */
+// show left menu
 hamburger.addEventListener('click', () => {
     leftMenu.classList.toggle('openMenu');
     hamburger.classList.toggle('open');
 });
 
+// hide left menu
 document.addEventListener('click', event => {
     if(!event.target.closest('.left-menu')) {
         leftMenu.classList.remove('openMenu');
@@ -16,6 +19,7 @@ document.addEventListener('click', event => {
     }
 });
 
+// show dropdown
 leftMenu.addEventListener('click', event => {
     const target = event.target;
     const dropdown = target.closest('.dropdown');
@@ -25,4 +29,16 @@ leftMenu.addEventListener('click', event => {
         leftMenu.classList.add('openMenu');
         hamburger.classList.add('open');
     }
+});
+
+// show new card image
+tvCardImg.forEach(el => {
+    let backdrop = el.dataset.backdrop;
+    let src = el.src;
+    el.addEventListener('mouseover', () => {
+        el.src = backdrop;
+    });
+    el.addEventListener('mouseout', () => {
+        el.src = src;
+    });
 });
